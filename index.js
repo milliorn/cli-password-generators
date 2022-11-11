d060fc60174b0b7911f56cc6afcfb39709e323b4
milliorn <scottmilliorn@gmail.com>
2022-11-10 17:06:27 -0800

    eslint

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-import chalk from "chalk";
-import clipboard from "clipboardy";
-import { program } from "commander";
-import createPassword from "./utils/createPassword.js";
-import savePassword from "./utils/savePassword.js";
+import chalk from 'chalk';
+import clipboard from 'clipboardy';
+import { program } from 'commander';
+import createPassword from './utils/createPassword.js';
+import savePassword from './utils/savePassword.js';
 
-program.version("1.0.0").description("Password Generator");
+program.version('1.0.0').description('Password Generator');
 
 program
-  .option("-l, --length <number>", "length of password", "8")
-  .option("-s, --save", "save password to passwords.txt")
-  .option("-nn, --no-numbers", "remove numbers")
-  .option("-ns, --no-symbols", "remove symbols")
-  .parse();
+    .option('-l, --length <number>', 'length of password', '8')
+    .option('-s, --save', 'save password to passwords.txt')
+    .option('-nn, --no-numbers', 'remove numbers')
+    .option('-ns, --no-symbols', 'remove symbols')
+    .parse();
 
 /**deconstruct options passed in */
 const { length, save, numbers, symbols } = program.opts();
@@ -19,7 +19,7 @@ const { length, save, numbers, symbols } = program.opts();
 const createdPassword = createPassword(length, numbers, symbols);
 
 if (save) {
-  savePassword(createdPassword);
+    savePassword(createdPassword);
 }
 
 /** copy to clipboard */
@@ -27,4 +27,4 @@ clipboard.writeSync(createdPassword);
 
 /**console.log(chalk.greenBright("Generated Password -> " + chalk.bold(createdPassword))); */
 
-console.log(chalk.yellow("Password copied to clipboard"));
+console.log(chalk.yellow('Password copied to clipboard'));

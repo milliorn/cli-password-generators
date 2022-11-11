6bf7a5b52b4e8021e2bc7e5a127f79380c2156cb
Scott Milliorn <scottmilliorn@gmail.com>
2022-11-10 17:40:19 -0800

    RandomSeed (#14)

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: 'eslint:recommended',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'windows'],
+    quotes: ['error', 'single'],
+    semi: [2, 'never'],
+  },
+}
--- utils/createPassword.js
+++ utils/createPassword.js
@@ -1,24 +1,29 @@
-const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const numbers = "0123456789";
-const symbols = "!@#$%^&*_-+=";
+import RandomSeed from 'random-seed'
+
+const alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const numbers = '0123456789'
+const symbols = '!@#$%^&*_-+='
 
 export default function createPassword(
   length = 8,
   hasNumbers = true,
   hasSymbols = true
 ) {
-  let chars = alpha;
-  hasNumbers ? (chars += numbers) : "";
-  hasSymbols ? (chars += symbols) : "";
-  return generatePassword(length, chars);
+  let chars = alpha
+  hasNumbers ? (chars += numbers) : ''
+  hasSymbols ? (chars += symbols) : ''
+  return generatePassword(length, chars)
 }
 
 const generatePassword = (length, chars) => {
-  let password = "";
+  let password = ''
+  /** https://github.com/skratchdot/random-seed */
+  const rand = RandomSeed.create()
 
   for (let i = 0; i < length; i++) {
-    password += chars.charAt(Math.random() * chars.length);
+    const n = rand(chars.length) // generate a random number
+    password += chars.charAt(n)
   }
 
-  return password;
-};
+  return password
+}

e49ae7c3952d795ec012490d7134caaea9068563
milliorn <scottmilliorn@gmail.com>
2024-02-14 15:44:51 -0800

    urfave hello world

--- golang/main.go
+++ golang/main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	// fmt.Println("Hello, World!")
+	app := &cli.App{
+		Name:  "hello",
+		Usage: "Print Hello World",
+		Action: func(c *cli.Context) error {
+			fmt.Println("Hello, World!")
+			return nil
+		},
+	}
+	app.Run([]string{"hello"})
 }

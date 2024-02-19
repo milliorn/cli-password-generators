db04c40ea34fc52302bf289d1165b3661e27f0ec
milliorn <scottmilliorn@gmail.com>
2024-02-19 00:26:06 -0800

    create password

--- golang/main.go
+++ golang/main.go
@@ -3,18 +3,21 @@ package main
 import (
 	"fmt" // Importing the 'fmt' package for formatted I/O operations
 	"log" // Importing the 'log' package for logging errors
-	"os"  // Importing the 'os' package for accessing command-line arguments and environment variables
+	"math/rand"
+	"os" // Importing the 'os' package for accessing command-line arguments and environment variables
 	"sort"
+	"time"
 
 	"github.com/urfave/cli/v2" // Importing the 'github.com/urfave/cli/v2' package for building CLI applications
 )
 
-const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const alpha_lower = "abcdefghijklmnopqrstuvwxyz"
+const alpha_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const numbers = "0123456789"
 const symbols = "!@#$%^&*_-+="
-const similar = "il1Lo0O"
-const vowels = "aeiouAEIOU"
-const consonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
+
+// const vowels = "aeiouAEIOU"
+// const consonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
 
 func main() {
 	cli.VersionFlag = &cli.BoolFlag{ // Setting the version flag of the CLI application
@@ -55,52 +58,80 @@ func main() {
 				Usage:   "Exclude special symbols from the password", // Setting the usage description of the flag
 			},
 
-			&cli.BoolFlag{
-				Name:    "no-uppercase",                                // Setting the name of the flag
-				Aliases: []string{"nu"},                                // Setting the aliases of the flag
-				Value:   false,                                         // Setting the default value of the flag
-				Usage:   "Exclude uppercase letters from the password", // Setting the usage description of the flag
-			},
+			// &cli.BoolFlag{
+			// 	Name:    "no-uppercase",                                // Setting the name of the flag
+			// 	Aliases: []string{"nu"},                                // Setting the aliases of the flag
+			// 	Value:   false,                                         // Setting the default value of the flag
+			// 	Usage:   "Exclude uppercase letters from the password", // Setting the usage description of the flag
+			// },
 
-			&cli.BoolFlag{
-				Name:    "no-lowercase",                                // Setting the name of the flag
-				Aliases: []string{"nl"},                                // Setting the aliases of the flag
-				Value:   false,                                         // Setting the default value of the flag
-				Usage:   "Exclude lowercase letters from the password", // Setting the usage description of the flag
-			},
+			// &cli.BoolFlag{
+			// 	Name:    "no-lowercase",                                // Setting the name of the flag
+			// 	Aliases: []string{"nl"},                                // Setting the aliases of the flag
+			// 	Value:   false,                                         // Setting the default value of the flag
+			// 	Usage:   "Exclude lowercase letters from the password", // Setting the usage description of the flag
+			// },
 
-			&cli.BoolFlag{
-				Name:    "no-similar",                                   // Setting the name of the flag
-				Aliases: []string{"nsi"},                                // Setting the aliases of the flag
-				Value:   false,                                          // Setting the default value of the flag
-				Usage:   "Exclude similar characters from the password", // Setting the usage description of the flag
-			},
+			// &cli.BoolFlag{
+			// 	Name:    "no-similar",                                   // Setting the name of the flag
+			// 	Aliases: []string{"nsi"},                                // Setting the aliases of the flag
+			// 	Value:   false,                                          // Setting the default value of the flag
+			// 	Usage:   "Exclude similar characters from the password", // Setting the usage description of the flag
+			// },
 
-			&cli.BoolFlag{
-				Name:    "no-vowels",                        // Setting the name of the flag
-				Aliases: []string{"nv"},                     // Setting the aliases of the flag
-				Value:   false,                              // Setting the default value of the flag
-				Usage:   "Exclude vowels from the password", // Setting the usage description of the flag
-			},
+			// &cli.BoolFlag{
+			// 	Name:    "no-vowels",                        // Setting the name of the flag
+			// 	Aliases: []string{"nv"},                     // Setting the aliases of the flag
+			// 	Value:   false,                              // Setting the default value of the flag
+			// 	Usage:   "Exclude vowels from the password", // Setting the usage description of the flag
+			// },
 
-			&cli.BoolFlag{
-				Name:    "no-consonants",                        // Setting the name of the flag
-				Aliases: []string{"nc"},                         // Setting the aliases of the flag
-				Value:   false,                                  // Setting the default value of the flag
-				Usage:   "Exclude consonants from the password", // Setting the usage description of the flag
-			},
+			// &cli.BoolFlag{
+			// 	Name:    "no-consonants",                        // Setting the name of the flag
+			// 	Aliases: []string{"nc"},                         // Setting the aliases of the flag
+			// 	Value:   false,                                  // Setting the default value of the flag
+			// 	Usage:   "Exclude consonants from the password", // Setting the usage description of the flag
+			// },
 
-			&cli.BoolFlag{
-				Name:    "no-letters",                        // Setting the name of the flag
-				Aliases: []string{"nle"},                     // Setting the aliases of the flag
-				Value:   false,                               // Setting the default value of the flag
-				Usage:   "Exclude letters from the password", // Setting the usage description of the flag
-			},
+			// &cli.BoolFlag{
+			// 	Name:    "no-letters",                        // Setting the name of the flag
+			// 	Aliases: []string{"nle"},                     // Setting the aliases of the flag
+			// 	Value:   false,                               // Setting the default value of the flag
+			// 	Usage:   "Exclude letters from the password", // Setting the usage description of the flag
+			// },
 		},
 		Action: func(cCtx *cli.Context) error { // Defining the action to be executed when the CLI application is run
-			// fmt.Println("Hello, World!") // Printing "Hello, World!" to the console
-			createPassword(cCtx) // Calling the 'createPassword' function with the context of the CLI application
-			return nil           // Returning nil to indicate successful execution of the action
+			length := cCtx.Int("length") // Getting the value of the 'length' flag
+			// no_consonants := cCtx.Bool("no-consonants") // Getting the value of the 'no-consonants' flag
+			// no_letters := cCtx.Bool("no-letters")       // Getting the value of the 'no-letters' flag
+			// no_lowercase := cCtx.Bool("no-lowercase")   // Getting the value of the 'no-lowercase' flag
+			no_numbers := cCtx.Bool("no-numbers") // Getting the value of the 'no-numbers' flag
+			// no_similar := cCtx.Bool("no-similar")       // Getting the value of the 'no-similar' flag
+			no_symbols := cCtx.Bool("no-symbols") // Getting the value of the 'no-symbols' flag
+			// no_uppercase := cCtx.Bool("no-uppercase") // Getting the value of the 'no-uppercase' flag
+			// no_vowels := cCtx.Bool("no-vowels")       // Getting the value of the 'no-vowels' flag
+
+			chars := alpha_lower + alpha_upper // Setting the characters to be used for generating the password
+
+			if !no_numbers {
+				chars += numbers
+			}
+
+			if !no_symbols {
+				chars += symbols
+			}
+
+			var password string
+
+			randSeed := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+			for i := 0; i < length; i++ {
+				n := randSeed.Intn(len(chars))
+				password += string(chars[n])
+			}
+
+			fmt.Println(password) // Printing the generated password
+			return nil            // Returning nil to indicate successful execution of the action
 		},
 	}
 
@@ -111,26 +142,4 @@ func main() {
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err) // Logging fatal errors
 	}
-}
-
-func createPassword(cCtx *cli.Context) {
-	length := cCtx.Int("length")                // Getting the value of the 'length' flag
-	no_numbers := cCtx.Bool("no-numbers")       // Getting the value of the 'no-numbers' flag
-	no_symbols := cCtx.Bool("no-symbols")       // Getting the value of the 'no-symbols' flag
-	no_uppercase := cCtx.Bool("no-uppercase")   // Getting the value of the 'no-uppercase' flag
-	no_lowercase := cCtx.Bool("no-lowercase")   // Getting the value of the 'no-lowercase' flag
-	no_similar := cCtx.Bool("no-similar")       // Getting the value of the 'no-similar' flag
-	no_vowels := cCtx.Bool("no-vowels")         // Getting the value of the 'no-vowels' flag
-	no_consonants := cCtx.Bool("no-consonants") // Getting the value of the 'no-consonants' flag
-	no_letters := cCtx.Bool("no-letters")       // Getting the value of the 'no-letters' flag
-
-	fmt.Println(length) // Printing the value of the 'length' flag
-	fmt.Println(no_numbers)
-	fmt.Println(no_symbols)
-	fmt.Println(no_uppercase)
-	fmt.Println(no_lowercase)
-	fmt.Println(no_similar)
-	fmt.Println(no_vowels)
-	fmt.Println(no_consonants)
-	fmt.Println(no_letters)
 }

fc98483c5676adf64e2b78d2889e93f3b78255c6
milliorn <scottmilliorn@gmail.com>
2024-09-03 12:22:15 -0700

    constants

--- python/app.py
+++ python/app.py
@@ -1,3 +1,14 @@
-import argparse 
-import os 
+import argparse
+import os
 
+
+APP_NAME = "CLI-Password Generator"
+LOWER_CHARS = "abcdefghijklmnopqrstuvwxyz"
+UPPER_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+NUMBER_CHARS = "0123456789"
+SYMBOL_CHARS = "!@#$%^&*_-+="
+ALPHA_CHARS = upperChars + lowerChars
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(description=APP_NAME)

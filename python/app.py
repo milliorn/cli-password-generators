67f969c285a133088a8f49acaa4e4236b143956f
milliorn <scottmilliorn@gmail.com>
2024-09-03 13:02:23 -0700

    add args, help

--- python/app.py
+++ python/app.py
@@ -7,8 +7,36 @@ LOWER_CHARS = "abcdefghijklmnopqrstuvwxyz"
 UPPER_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 NUMBER_CHARS = "0123456789"
 SYMBOL_CHARS = "!@#$%^&*_-+="
-ALPHA_CHARS = upperChars + lowerChars
+ALPHA_CHARS = UPPER_CHARS + LOWER_CHARS
+DEFAULT_LENGTH = 8
+DEFAULT_BOOL = False
 
 
 def create_parser():
     parser = argparse.ArgumentParser(description=APP_NAME)
+
+    parser.add_argument(
+        "-l", "--length", help="Length of the password", default=DEFAULT_LENGTH)
+    parser.add_argument("-nm", "--no-numbers",
+                        help="Exclude numbers from the password", default=DEFAULT_BOOL)
+    parser.add_argument("-ns", "--no-symbols",
+                        help="Exclude special symbols from the password", default=DEFAULT_BOOL)
+    parser.add_argument("-nu", "--no-uppercase",
+                        help="Exclude uppercase letters from the password", default=DEFAULT_BOOL)
+    parser.add_argument("--nl", "--no-lowercase",
+                        help="Exclude lowercase letters from the password", default=DEFAULT_BOOL)
+    parser.add_argument("--nle", "--no-letters",
+                        help="Exclude letters from the password", default=DEFAULT_BOOL)
+
+    return parser
+
+
+def main():
+    parser = create_parser()
+    args = parser.parse_args()
+
+    parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
